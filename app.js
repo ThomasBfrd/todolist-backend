cd8009482b49edda3abc3fca67d71e3b59fb4859
ThomasBfrd <thomas.bfrd@gmail.com>
2023-10-11 20:41:30 +0200

    Correctifs app.js

--- app.js
+++ app.js
@@ -19,24 +19,49 @@ mongoose.connect(mongoKey,
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Max-Age", "1800");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+//   );
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "default-src 'none'; font-src 'self' https://fonts.googleapis.com;"
+//   );
+//   next();
+// });
+
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'none'; font-src 'self' https://fonts.googleapis.com;"
-  );
-  next();
-});
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+
 
 app.use((error, req, res, next) => {
   console.error(error);
@@ -48,4 +73,5 @@ app.use(express.json());
 app.use("/tasks", stuffRoutes);
 app.use("/auth", userRoutes);
 
+module.exports = allowCors(handler)
 module.exports = app;
